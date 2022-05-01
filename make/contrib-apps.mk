f0560e9be44e39fcd3a7a3fb755c6f6290aada60
Greder <s12345ab@gmail.com>
2022-05-01 20:12:43 +0300

    update wget 1.21.3

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -1089,8 +1089,9 @@ $(D)/avahi: $(D)/bootstrap $(D)/expat $(D)/libdaemon $(D)/dbus $(ARCHIVE)/$(AVAH
 #
 # wget
 #
-WGET_VER = 1.19.5
+WGET_VER = 1.21.3
 WGET_SOURCE = wget-$(WGET_VER).tar.gz
+WGET_PATCH = wget-$(WGET_VER).patch
 
 $(ARCHIVE)/$(WGET_SOURCE):
 	$(WGET) https://ftp.gnu.org/gnu/wget/$(WGET_SOURCE)
@@ -1100,6 +1101,7 @@ $(D)/wget: $(D)/bootstrap $(D)/openssl $(ARCHIVE)/$(WGET_SOURCE)
 	$(REMOVE)/wget-$(WGET_VER)
 	$(UNTAR)/$(WGET_SOURCE)
 	$(CHDIR)/wget-$(WGET_VER); \
+		$(call apply_patches, $(WGET_PATCH)); \
 		$(CONFIGURE) \
 			--prefix=/usr \
 			--mandir=/.remove \
