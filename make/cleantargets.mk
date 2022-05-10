4e217bd6baa49f92abef012773e72ffae30ffd1d
Greder <35959120+Greder@users.noreply.github.com>
2022-05-10 17:14:37 +0300

    Update cleantargets.mk

--- make/cleantargets.mk
+++ make/cleantargets.mk
@@ -8,7 +8,7 @@ clean:
 	@-$(MAKE) tools-clean
 	@-rm -rf $(RELEASE_DIR)
 	@-rm -rf $(TARGET_DIR)
-	@-rm -rf $(HOST_DIR)
+	@-rm -rf $(SOURCE_DIR)
 	@-rm -rf $(BOOT_DIR)
 	@-rm -rf $(D)/kernel
 	@-rm -rf $(D)/*.do_compile
@@ -17,21 +17,19 @@ clean:
 	@-rm -rf $(D)/directories
 	@-rm -rf $(D)/diverse-tools
 	@-rm -rf $(D)/system-tools
-	@-rm -rf $(D)/*
-	touch $(D)/crosstool
-ifeq ($(BOXARCH),sh4)	
-	touch $(D)/crosstool-rpminstall
-endif
 	@echo -e "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 distclean: depsclean
 	@echo -e "$(TERM_YELLOW)---> cleaning whole build system .. $(TERM_NORMAL)"
-	@-$(MAKE) kernel-clean
-	@-$(MAKE) driver-clean
-	@-rm -rf $(TUFSBOX_DIR)
+	@-rm -rf $(RELEASE_DIR)
+	@-rm -rf $(TARGET_DIR)
+	@-rm -rf $(HOST_DIR)
+	@-rm -rf $(BOOT_DIR)
+	@-rm -rf $(CROSS_DIR)
+	@-rm -rf $(BUILD_TMP)
+	@-rm -rf $(SOURCE_DIR)
+	@-rm -rf $(D)
 	@echo -e "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 %-clean:
 	( cd $(D) && find . -name $(subst -clean,,$@) -delete )
-
-
